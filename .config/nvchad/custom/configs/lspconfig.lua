ed7108b7d7492467f33de6b68e45aa98b81869ee
Kaj Fehlhaber <kaj.fehlhaber@wirelesscar.com>
2023-09-11 19:45:15 +0200

    C# LSP support for Neovim

--- .config/nvchad/custom/configs/lspconfig.lua
+++ .config/nvchad/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 local configs = require "lspconfig.configs"
 local util = require "lspconfig.util"
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "terraformls", "bashls", "bufls"}
+local servers = { "html", "cssls", "tsserver", "terraformls", "bashls", "bufls","csharp_ls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
