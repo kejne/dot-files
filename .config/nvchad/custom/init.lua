2d81535292d6cf2184a2ece709a9d4305e30201b
Kaj Fehlhaber <kaj.fehlhaber@wirelesscar.com>
2023-09-20 12:40:43 +0200

    Use system clipboard in neovim

--- .config/nvchad/custom/init.lua
+++ .config/nvchad/custom/init.lua
@@ -31,17 +31,4 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt.expandtab = true
   end,
   })
--- local cmd = vim.cmd
--- -- local autocmd = vim.api.nvim_create_autocmd
---
--- -- Auto resize panes when resizing nvim window
--- -- autocmd("VimResized", {
--- --   pattern = "*",
--- --   command = "tabdo wincmd =",
--- -- })
--- cmd([[
--- 	augroup CustomHI
--- 		autocmd!
--- 		autocmd VimEnter * NoMatchParen 
--- 	augroup END
--- ]])
+vim.api.nvim_set_option("clipboard","unnamed")
