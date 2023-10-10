0a396cfbbe3543786a37c7aaa0681e80c5728617
Kaj Fehlhaber <kaj.fehlhaber@wirelesscar.com>
2023-10-10 19:50:46 +0200

    nvim setting improvements

--- .config/nvchad/custom/plugins.lua
+++ .config/nvchad/custom/plugins.lua
@@ -30,8 +30,7 @@ end
 
 ---@type NvPluginSpec[]
 local plugins = {
-  {
-    'kevinhwang91/nvim-ufo',
+  {  'kevinhwang91/nvim-ufo',
     lazy = false,
     requires = 'kevinhwang91/promise-async',
     config = function()
@@ -43,17 +42,13 @@ local plugins = {
        })
     end,
   },
-  -- Override plugin definition options
-  {
-    'kevinhwang91/promise-async',
+  {    'kevinhwang91/promise-async',
     lazy = true,
   },
-  {
-    "nvim-lua/plenary.nvim",
+  {    "nvim-lua/plenary.nvim",
     lazy = true,
   },
-  {
-    "ThePrimeagen/refactoring.nvim",
+  {  "ThePrimeagen/refactoring.nvim",
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -70,8 +65,8 @@ local plugins = {
       })
     end,
   },
-  {
-    "ThePrimeagen/harpoon", lazy = false,
+  { "ThePrimeagen/harpoon",
+    lazy = false,
     config = function()
       require("harpoon").setup({
         tabline = true,
@@ -80,8 +75,7 @@ local plugins = {
       })
     end
   },
-  {
-    "NeogitOrg/neogit",
+  {    "NeogitOrg/neogit",
     keys = {
       { "<leader>gg", ":Neogit kind=vsplit<CR>", desc = "Neogit split" },
     },
@@ -93,15 +87,16 @@ local plugins = {
     },
     config = true
   },
-  {
-    "tpope/vim-fugitive", lazy = false
+  {    "tpope/vim-fugitive",
+    lazy = false
   },
-  {
-    "nvim-neorg/neorg",
+  {    "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      { "<leader>ow", ":Neorg workspace ", desc = "Neorg workspace" },
+      { "<leader>oi", ":Neorg index<CR>", desc = "Neorg index" },
+      { "<leader>oj", ":Neorg journal today<CR>", desc = "Neorg journal" },
+      { "<leader>ok", ":Neorg journal ", desc = "Neorg journal" },
     },
     -- ft = "norg",
     config = function()
@@ -115,14 +110,14 @@ local plugins = {
                 notes = "~/second-brain",
                 work = "~/work-brain",
               },
+              default_workspace = "notes"
             },
           },
         },
       }
     end,
   },
-  {
-    "rest-nvim/rest.nvim",
+  {    "rest-nvim/rest.nvim",
     keys = {
       { "<leader>rt", "<Plug>RestNvim", desc = "Rest Call" },
     },
@@ -164,8 +159,7 @@ local plugins = {
       })
     end
   },
-  {
-    "neovim/nvim-lspconfig",
+  {    "neovim/nvim-lspconfig",
     dependencies = {
       -- format & linting
       {
@@ -180,26 +174,19 @@ local plugins = {
       require "custom.configs.lspconfig"
     end, -- Override to setup mason-lspconfig
   },
-  {
-    "fatih/vim-go",
+  {    "fatih/vim-go",
     ft = "go",
   },
-  {
-    "towolf/vim-helm",
-    ft = "yml",
+  {    "towolf/vim-helm",
+    ft = {"yml","yaml"},
   },
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
+  {    "williamboman/mason.nvim",
     opts = overrides.mason
   },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
+  {    "nvim-treesitter/nvim-treesitter",
     opts = overrides.treesitter,
   },
-  {
-      "nvim-neo-tree/neo-tree.nvim",
+  {      "nvim-neo-tree/neo-tree.nvim",
       lazy = false,
       branch = "v3.x",
       dependencies = {
@@ -208,15 +195,13 @@ local plugins = {
         "MunifTanjim/nui.nvim",
       }
   },
-  {
-    "max397574/better-escape.nvim",
+  {    "max397574/better-escape.nvim",
     event = "InsertEnter",
     config = function()
       require("better_escape").setup()
     end,
   },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+  {    "nvim-treesitter/nvim-treesitter-textobjects",
     after = "nvim-treesitter",
     requires = "nvim-treesitter/nvim-treesitter",
     lazy = false,
@@ -230,42 +215,20 @@ local plugins = {
             lookahead = true,
 
             keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
               ["af"] = "@function.outer",
               ["if"] = "@function.inner",
               ["ii"] = "@conditional.inner",
               ["ai"] = "@conditional.outer",
               ["il"] = "@loop.inner",
               ["al"] = "@loop.outer",
-              ["ac"] = "@class.outer",
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
               ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              -- You can also use captures from other query groups like `locals.scm`
               ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
             },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
             selection_modes = {
               ['@parameter.outer'] = 'v', -- charwise
               ['@function.outer'] = 'V', -- linewise
               ['@class.outer'] = '<c-v>', -- blockwise
             },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true of false
-            -- include_surrounding_whitespace = true,
           },
         },
       }
