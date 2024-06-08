973934b07a71e144c8fea239dfed543a7095ca6c
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2024-06-08 12:25:21 +0200

    add initial marble config

---
+++ install-marble.sh
@@ -0,0 +1,7 @@
+echo 'Section "InputClass"
+        Identifier  "Marble Mouse"
+        MatchProduct "Logitech USB Trackball"
+        Driver "libinput"
+        Option "ScrollMethod" "button"
+        Option "ScrollButton" "8"
+EndSection'
