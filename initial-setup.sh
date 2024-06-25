ca6549e82b48c67f6fecc72fd7ab835a80823ba7
Kaj Fehlhaber <kaj.fehlhaber@wirelesscar.com>
2024-06-25 08:29:52 +0200

    updated init script

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,30 +1,65 @@
-#/bin/bash
+# #/bin/bash
+# 
+# echo "####################################################"
+# echo "#   1. Updating system                             #"
+# echo "####################################################"
+# wget -qO - https://regolith-desktop.org/regolith.key | \
+# gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+# 
+# echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+# https://regolith-desktop.org/release-3_1-ubuntu-jammy-amd64 jammy main" | \
+# sudo tee /etc/apt/sources.list.d/regolith.list
+# 
+# sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
+# 
+# echo "####################################################"
+# echo "#   2. Installing packages                         #"
+# echo "####################################################"
+# 
+# sudo apt install p7zip-full libsecret-tools git python3 python3-pip python-is-python3 zsh curl fonts-powerline flameshot build-essential regolith-desktop regolith-session-flashback regolith-look-gruvbox i3xrocks-battery i3xrocks-memory gpg pass ripgrep -y
+# 
+# sudo snap install code obsidian --classic
+# sudo snap install --beta nvim --classic
+# sudo snap install slack firefox spotify
+# 
+# echo "####################################################"
+# echo "#   3. Setting up zsh                              #"
+# echo "####################################################"
+# curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+# cd ~/.local/bin
+# ln -s ~/.local/kitty.app/bin/kitty
+# ln -s ~/.local/kitty.app/bin/kitten
+# 
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# brew install gcc starship kubectl gh golang helm fzf zoxide rust bat
+# 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# gh auth login
+# cd ~
+# mkdir -p git/kaj
+# cd git/kaj
+# gh repo clone kejne/dot-files
 
-echo "####################################################"
-echo "#   1. Updating system                             #"
-echo "####################################################"
-
-sudo apt update -y && sudo apt upgrade -y && sudo apt-dist-upgrade -y && sudo apt autoremove -y
-
-echo "####################################################"
-echo "#   2. Installing packages                         #"
-echo "####################################################"
-
-sudo apt install p7zip-full libsecret-tools git openjdk-11-jdk maven python3 python3-pip python-is-python3 zsh curl fonts-powerline flameshot fzf autojump -y
-
-sudo snap install code --classic
-
-wget -q -O - https://repo.protonvpn.com/debian/public_key.asc | sudo apt-key add -
-sudo add-apt-repository 'deb https://repo.protonvpn.com/debian unstable main'
-sudo apt-get update && sudo apt-get install protonvpn -y
-
-echo "####################################################"
-echo "#   3. Setting up zsh                              #"
-echo "####################################################"
-
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-rm ~/.zshrc
+rm ~/.zshrc || echo ".zshrc already cleaned"
+mkdir -p ~/.config/dotfiles/
+cd ~/.config/dotfiles/
+ln -s ~/git/kaj/dot-files personal
 cd ~
-ln -s ~/.myconfig/ubuntu-setup/.zshrc
-cp ~/.myconfig/ubuntu-setup/agnoster-kejne.zsh-theme ~/.oh-my-zsh/themes/
+ln -s ~/.config/dotfiles/personal/.zshrc
+cd ~/.config
+rm -rf regolith3
+ln -s ~/.config/dotfiles/personal/starship.toml
+ln -s ~/.config/dotfiles/personal/regolith2 regolith3
+ln -s ~/.config/dotfiles/personal/nvim
+
+
+cd ~/git/kaj
+gh repo clone kejne/second-brain
+mkdir -p ~/second-brain/default
+
+cd ~/second-brain
+ln -s ~/git/kaj/second-brain personal
+
+env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
+go install github.com/erikkinding/sk
