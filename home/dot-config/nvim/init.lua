df8a9ca0ec4f61b11fa917d3d096b80390d9cb42
Kaj Fehlhaber <kaj.fehlhaber@wirelesscar.com>
2025-06-11 08:41:10 +0200

    chore: alter deprecated config

--- home/dot-config/nvim/init.lua
+++ home/dot-config/nvim/init.lua
@@ -1480,19 +1480,24 @@ require('lazy').setup {
       -- for example
       provider = 'copilot',
       -- provider = 'claude',
-      claude = {
-        endpoint = 'https://api.anthropic.com',
-        model = 'claude-3-5-haiku-20241022',
-        temperature = 0,
-        max_tokens = 4096,
-      },
-      openai = {
-        endpoint = 'https://api.openai.com/v1',
-        model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- timeout in milliseconds
-        temperature = 0, -- adjust if needed
-        max_tokens = 4096,
-        -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      providers = {
+        claude = {
+          endpoint = 'https://api.anthropic.com',
+          model = 'claude-3-5-haiku-20241022',
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 8096,
+          },
+        },
+        openai = {
+          endpoint = 'https://api.openai.com/v1',
+          model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+          timeout = 30000, -- timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 8096,
+          },
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
